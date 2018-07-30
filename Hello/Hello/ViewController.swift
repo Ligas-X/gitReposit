34ef71d88c30a30f2deaccc5c96611267c2a9a8e
Ligas <Ligasy@gmail.com>
2018-07-30 21:33:32 +0500

    Добавлены размеры иконок для Xcode

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Agil Ignis on 30.07.2018.
+//  Copyright © 2018 Agil Ignis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
