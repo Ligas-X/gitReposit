fd46453a04c411d37b046d51a33186180660aac4
Ligas <Ligasy@gmail.com>
2018-08-05 20:30:39 +0500

    Тестовое приложение на Swift для Apple Watch 3 (watchOS 4.2)

---
+++ Hello on iWatch/Hello on iWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello on iWatch
+//
+//  Created by Agil Ignis on 05.08.2018.
+//  Copyright © 2018 Agil Ignis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
